efba2c093b11e1d9215fa13bdc236519d4e67532
OmarFayaz <omarfayaz@ucla.edu>
2026-04-09 17:31:56 -0700

    Task 2: fix pose update, densify path, 610mm square

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -84,13 +84,13 @@ def run(robot: Robot) -> None:
             print("[FSM] INIT (odometry reset)")
             path_control_points = [ #Define your path control points here (x, y) in mm
                 (0.0, 0.0), # 1st point
-                (0.0, 500.0), # 2nd point
-                (500.0, 500.0), # 3rd point
-                (500.0, 0.0), # 4th point
+                (0.0, 610.0), # 2nd point
+                (610.0, 610.0), # 3rd point
+                (610.0, 0.0), # 4th point
                 (0.0, 0.0), # 5th point
             ]    
-            path1 = path_control_points
-            #path1 = densify_polyline(path_control_points, spacing=20.0)
+            #path1 = path_control_points
+            path1 = densify_polyline(path_control_points, spacing=20.0)
             remaining_path = path1.copy() 
             print("Path is ready, Entering IDLE state.")
             state = "IDLE"
